f370ec0e95b9113cab117e5f790c1550f2ad2ed1
David Maurer <davemaurermail@gmail.com>
2015-12-08 14:41:31 -0700

    Adjust tests to remove unneeded rows and columns attributes
    rename title to Pomododge in index.html

--- test/playfield-test.js
+++ test/playfield-test.js
@@ -6,38 +6,25 @@ const Playfield = require('../lib/playfield'); //can leave off the .js, just lik
 describe('Playfield', function() {
   it('should instantiate a new playfield', function () {
     let playfield = new Playfield();
+
     assert.isObject(playfield);
-  });
-
-  it('should have a columns property based on the first argument', function () {
-    let playfield = new Playfield(20, 20);
-    assert.equal(20, playfield.columns)
-  });
-
-  it('should have a rows property based on the second argument', function () {
-    let playfield = new Playfield(20, 20);
-    assert.equal(20, playfield.rows);
-  });
-
-  it('should default to 20 columns', function () {
-    let playfield = new Playfield();
-    assert.equal(20, playfield.columns);
-  });
-
-  it('should default to 20 rows', function () {
-    let playfield = new Playfield();
-    assert.equal(20, playfield.rows);
   });
 
   it('should start with an array of tomatoes', function () {
     let playfield = new Playfield();
+
     assert.isArray(playfield.tomatoes);
   });
 });
 
 describe('addTomato', function () {
-  it('should add a new block to its block array', function () {
+  it('should add a new tomato to its tomato array', function () {
     let playfield = new Playfield();
 
+    assert.equal(playfield.tomatoes.count, 0);
+
+    playfield.addTomato(-5, -5);
+
+    assert.equal(playfield.tomatoes.count, 1);
   })
 });
--- test/tomato-test.js
+++ test/tomato-test.js
@@ -5,28 +5,30 @@ const Tomato = require('../lib/tomato');
 
 describe('Tomato', function () {
   beforeEach(function () {
-    this.playfield = new Playfield(); // using beforeEach to add this to every test we write within this tomato. There is also
-                                      // an afterEach.
+    this.playfield = new Playfield();
   });
 
   it('should have a reference to the playfield', function () {
     let tomato = new Tomato(this.playfield);
+
     assert.equal(tomato.playfield, this.playfield);
   });
-  // would like playfield to add tomato, not pass playfield into the tomato
 
   it('should have an X-coord', function () {
     let tomato = new Tomato(this.playfield, 5, 5);
+
     assert.strictEqual(tomato.x, 5);
   });
 
   it('should have a Y-coord', function () {
     let tomato = new Tomato(this.playfield, 5, 5);
+
     assert.strictEqual(tomato.y, 5);
   });
 
   it('should be included in the playfield\'s array of tomatoes', function () {
     let tomato = new Tomato(this.playfield, 5, 5);
+
     assert.include(this.playfield.tomatoes, tomato);
   });
 });
