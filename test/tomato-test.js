22b4a46cb8709c766dcd69be99ea3991ed7f101b
Erik Butcher <with_a_k@mac.com>
2015-12-14 14:10:17 -0700

    Add methods on tomato necessary for motion

--- test/tomato-test.js
+++ test/tomato-test.js
@@ -33,4 +33,23 @@ describe('Tomato', function () {
 
     assert.include(this.game.tomatoes, tomato1);
   });
+
+  it('should have a direction', function () {
+    assert.strictEqual(this.tomato1.direction, 0);
+  });
+
+  it('should have a speed', function () {
+    assert.strictEqual(this.tomato1.speed, 3);
+  });
+
+  it('should have a velocity', function () {
+    assert.strictEqual(this.tomato1.velocity().xv, 3);
+    assert.strictEqual(this.tomato1.velocity().yv, 0);
+  });
+
+  it('should update its own position', function () {
+    assert.deepEqual(this.tomato1.center, {x: 500, y: 100});
+    this.tomato1.updatePosition();
+    assert.deepEqual(this.tomato1.center, {x: 503, y: 100});
+  });
 });
